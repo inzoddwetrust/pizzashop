0ab8af7b7ad4c2a60cef9c69d5949a113d8d7dec
inzoddwetrust <inzoddwetrust@gmail.com>
2017-04-29 04:39:46 +0600

    Total items count added

--- public/app.js
+++ public/app.js
@@ -10,5 +10,20 @@ function something()
 
 function add_to_cart(id)
 {
-  alert('You added pizza with id: ' + id);
+  var key = 'product_' + id,
+      total=0,
+      x = window.localStorage.getItem(key);
+
+  x=x*1+1;
+  window.localStorage.setItem(key, x);
+
+  var ls_keys = Object.keys(localStorage),
+      i = ls_keys.length;
+
+  while (i--) {
+    total=total+localStorage.getItem(ls_keys[i])*1;
+  }
+
+
+  alert (key + ' ' + x + ' total: ' + total);
 }
